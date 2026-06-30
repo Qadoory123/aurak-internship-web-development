68d098283eaf1d39f944b703f25356520e7eee08
Abdalqader Ahmed <abdalqaderahmad2021@gmail.com>
2026-06-30 12:37:47 +0400

    Refactor App component to include Navbar and ThemeProvider

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,25 +1,38 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 import Home from "./pages/Home";
 import UserDetail from "./pages/UserDetail";
 import About from "./pages/About";
 import "./App.css";
 
+function Navbar() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <nav className={`navbar ${theme}`}>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <button onClick={toggleTheme}>
+        {theme === "light" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+      </button>
+    </nav>
+  );
+}
+
 function App() {
   return (
-    <BrowserRouter>
-      <nav className="navbar">
-        <Link to="/">User Directory</Link>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </div>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users/:id" element={<UserDetail />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users/:id" element={<UserDetail />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
