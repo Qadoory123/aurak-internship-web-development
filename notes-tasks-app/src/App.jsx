98d55ee7623df58e5e718fc478c82c4f078f8437
Abdalqader Ahmed <abdalqaderahmad2021@gmail.com>
2026-07-03 14:35:10 +0400

    Refactor App component to use AppShell

--- notes-tasks-app/src/App.jsx
+++ notes-tasks-app/src/App.jsx
@@ -1,21 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { TaskProvider } from "./context/TaskContext";
+import { TaskProvider, useTasks } from "./context/TaskContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import TaskDetail from "./pages/TaskDetail";
 import AddTask from "./pages/AddTask";
 import "./App.css";
 
-function App() {
+function AppShell() {
+  const { theme } = useTasks();
   return (
-    <TaskProvider>
-      <BrowserRouter>
-        <Navbar />
+    <div className={`app-shell ${theme}`}>
+      <Navbar />
+      <div className="page-container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tasks/:id" element={<TaskDetail />} />
           <Route path="/add" element={<AddTask />} />
         </Routes>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <TaskProvider>
+      <BrowserRouter>
+        <AppShell />
       </BrowserRouter>
     </TaskProvider>
   );
